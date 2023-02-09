bd545f00ce2dd3414775d124e2eea8239096113b
mahic <mahic@domain.com>
2023-02-08 21:15:14 -0700

    Finished w21a

--- app.py
+++ app.py
@@ -12,6 +12,46 @@ def get_items():
     else:
         return "Something went wrong!"
 
+
+@app.post('/api/item')
+def post_item():
+    name = request.json.get('name')
+    description = request.json.get('description')
+    quantity = request.json.get('quantity')
+    result = run_statement("CALL post_item(?,?,?)", [name, description, quantity])
+    if result[0][0] == 1:
+        return "Item added successfully!"
+    else:
+        "Something went wrong"
+
+@app.patch('/api/item')
+def patch_item():
+    quantity = request.json.get('quantity')
+    id = request.json.get('itemId')
+    result = run_statement("CALL patch_item(?,?)" [quantity, id])
+    if result [0][0] == 1:
+        return "Item quantity updated successfully!"
+    else:
+        "Something went wrong"
+
+
+@app.delete('/api/item')
+def delete_item():
+    id = request.json.get('itemId')
+    result = run_statement("CAll delete_item(?)", [id])
+    if(type(result) == list):
+        if result[0][0] == 1:
+            return "Item deleted successfully!"
+        else:
+            return "Item {} does not exist!".format(id)
+    else:
+        "Something went wrong"
+
+
+
+# !.................EMPLOYEE.............
+
+
 @app.get('/api/employee')
 def get_employee():
     id_input = request.json.get('employeeId')
@@ -21,66 +61,41 @@ def get_employee():
     else:
         return "Something went wrong!"
 
-@app.post('/api/item')
-def post_item():
-    name = request.json.get('name')
-    description = request.json.get('description')
-    quantity = request.json.get('quantity')
-    result = run_statement("CALL post_item(?,?,?)", [name, description, quantity])
-    if result == None:
-        return "Success"
-    else:
-        "Something went wrong"
 
 @app.post('/api/employee')
 def post_employee():
     name = request.json.get('name')
     hourly_wage = request.json.get('hourlyWage')
     result = run_statement("CALL post_employee(?,?)" [name, hourly_wage])
-    if result == None:
-        return "Success"
+    # first field of the first array
+    if result[0][0] == 1:
+        return "New employee's name and hourly wage added successfully!"
     else:
         "Something went wrong"
 
-@app.patch('/api/item')
-def patch_item():
-    quantity = request.json.get('quantity')
-    id = request.json.get('itemId')
-    result = run_statement("CALL patch_item(?,?)" [quantity, id])
-    if result == None:
-        return "Success"
-    else:
-        "Something went wrong"
 
 @app.patch('/api/employee')
 def patch_employee():
     hourly_wage = request.json.get('hourlyWage')
     id = request.json.get('employeeId')
     result = run_statement("CALL patch_employee(?,?)" [hourly_wage, id])
-    if result == None:
-        return "Success"
+    if result[0][0] == 1:
+        return "Employee's hourly wage updated successfully!"
     else:
         "Something went wrong"
 
-@app.delete('/api/item')
-def delete_item():
-    id = request.json.get('itemId')
-    result = run_statement("CAll delete_item(?)", [id])
-    if result == None:
-        return "Success"
-    else:
-        "Something went wrong"
 
 @app.delete('/api/employee')
 def delete_empoyee():
     id = request.json.get('employeeId')
     result = run_statement("CAll delete_employee(?)", [id])
-    if result == None:
-        return "Success"
+    if(type(result) == list):
+        if result[0][0] == 1:
+            return "Employee deleted successfully!"
+        else:
+            return "Employee {} does not exist!".format(id)
     else:
         "Something went wrong"
-
-
 
 
 
